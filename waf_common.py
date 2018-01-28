52d426b8521e8ef6ad86f0c2e0107b874b33340e
Alex Rusev <hou.alexx@gmail.com>
2018-01-27 17:45:34 -0800

    Updated for 16.5.323 and  VS2017 + VC140 toolchain

--- waf_common.py
+++ waf_common.py
@@ -1,10 +1,13 @@
 import os
 from waflib.Configure import conf
+# VC140 toolchain missing this tool which Microsoft decided to move here:
+path_to_mt = r'C:/Program Files (x86)/Windows Kits/10/bin/10.0.16299.0/x64'
 
 @conf
 def setup_houdini(ctx):
+    os.environ['PATH'] = os.environ['PATH'] + os.pathsep + path_to_mt
     ctx.load('msvc')
-    ctx.env.HFS = os.getenv("DEV_HFS")
+    ctx.env.HFS = os.getenv("HFS")
     ctx.env.HFS_LIB = os.path.join(ctx.env.HFS, 'custom/houdini/dsolib')
     ctx.env.HFS_INC = os.path.join(ctx.env.HFS, 'toolkit/include')
     ctx.env.CXXFLAGS = ['-nologo', '-TP', '-Zc:forScope', '-wd4355', '-w14996', '-O2', '-MD', '-EHsc', '-GR',
@@ -12,7 +15,7 @@ def setup_houdini(ctx):
     ctx.env.DEFINES = ['MAKING_DSO', 'NDEBUG', 'I386', 'WIN32', 'SWAP_BITFIELDS', '_WIN_32_WINNT=0X0502',
                         'NOMINMAX', 'STRICT', 'WIN32_LEAN_AND_MEAN', '_USE_MATH_DEFINES',
                         '_CRT_SECURE_NO_DEPRICATE', '_CRT_SECURE_NO_WARNINGS', '_CRT_NONSTDC_NO_DEPRECATE', '_SCL_SECURE_NO_WARNINGS',
-                        'BOOST_ALL_NO_LIB', 'SESI_LITTLE_ENDIAN', 'AMD64', 'SIZEOF_VOID_P=8', 'FBX_ENABLED=1',
+                        'HBOOST_ALL_NO_LIB', 'SESI_LITTLE_ENDIAN', 'AMD64', 'SIZEOF_VOID_P=8', 'FBX_ENABLED=1',
                         'OPENCL_ENABLED=1', 'OPENVDB_ENABLED=1']
 
     hlibs = ctx.env.HFS_LIB
--- wscript
+++ wscript
@@ -1,23 +1,25 @@
 import os
 from waf_common import setup_houdini
 
-HHOME = r'C:/Users/alex/Documents/houdini16.0'
+HHOME = r'C:\Users\houal\OneDrive\Documents\houdini16.5'
 DSO_HOME = os.path.join(HHOME, 'DSO')
 
 
 def configure(conf):
-    conf.setup_houdini()
+	conf.env.MSVC_VERSIONS = ['msvc 14.0']
+	conf.env.MSVC_TARGETS = ['x86_amd64']
+	conf.setup_houdini()
 
 
 def build(ctx):
-	ctx.objects(source="hreeble\Element.cpp", 
+	ctx.objects(source="src\Element.cpp", 
 				target="objects",
-				includes=['hreeble', ctx.env.HFS_INC],
+				includes=['src', ctx.env.HFS_INC],
 				defines=ctx.env.DEFINES)
 
-	ctx.shlib(source="hreeble\sop_hreeble.cpp",
+	ctx.shlib(source="src\sop_hreeble.cpp",
 				target='hreeble',
-				includes=['hreeble', ctx.env.HFS_INC],
+				includes=['src', ctx.env.HFS_INC],
 				defines=ctx.env.DEFINES,
 				use="objects")
 	ctx.install_files(DSO_HOME, ['hreeble.dll'])
